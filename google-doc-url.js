32a0cfc1917133577ff00c2e3b07626ee2a711c1
Marin County Breeding Bird Atlas <77134855+mcbba@users.noreply.github.com>
2022-05-23 12:20:45 -0700

    Initial commit

---
+++ google-doc-url.js
@@ -0,0 +1,3 @@
+var googleDocURL = 'https://docs.google.com/spreadsheets/d/1xiVsJyZgNXziuB4ZcCw-_Pww6gfLpF3wsrYWIh6dLp4/edit#gid=0';
+var googleApiKey = 'AIzaSyCq3m304-zo15BPsOtlR3BnHsClkaCYBxg';
+// new key for 2022: AIzaSyAJ2ThY0Xqtjl4MXohW-AhgqwfrctgT5zw
